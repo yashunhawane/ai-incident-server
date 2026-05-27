d17287377d7ec5a3859d604099271999cb7f9871
yashunhawane <yashunhawane@gmail.com>
2026-05-27 16:21:21 +0530

    cors prod url

--- src/app.js
+++ src/app.js
@@ -9,7 +9,16 @@ import uploadRoutes from './routes/uploadRoutes.js';
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://ai-incident-frontend.vercel.app",
+      "http://localhost:3000",
+    ],
+   
+  })
+);
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
